f53ef5a7d31c3ec7d9c6ba9785c1ed6b6f61fb20
joeymou <564449571@qq.com>
2016-04-04 15:29:51 -0400

    mainmemu.php updated

--- mainmenu.php
+++ mainmenu.php
@@ -76,10 +76,9 @@
 				$topics=array(); 
 				//Set the Array to call the function (this function is in internal_api.php)
 				$topics=getTopics();
-
 				//Loop through the results and display them.. you can write out HTML with them as you see I'm writing out the topic and then the BR HTML tag.  If you need help concatenating the tags let me know
 				foreach($topics as $item) {
-				    echo '<li><a href="#">' . $item['topic'] . '</a></li>';
+            echo '<li><a href=topic.php?id=' . $item["topicID"] . '>' . $item["topic"] . '</a></li>';
 				}
             ?>
           </ul>
