9ca2ba59eedb897eb3b45a5c3fd0b9e271586c50
Patrick Hill <graffixnyc@gmail.com>
2016-04-15 08:22:51 -0400

    added responsive menu support

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <html >
   <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>LearningX | Your perfect Java teacher</title>
     
     
--- topic.php
+++ topic.php
@@ -15,6 +15,7 @@
 <html >
   <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>LearningX | Your perfect Java teacher</title>
     <link rel="stylesheet" href="css/normalize.css">
     <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
