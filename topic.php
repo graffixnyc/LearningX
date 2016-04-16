f1497a09044be72b48db3027433a369b38c76f5d
Patrick Hill <graffixnyc@gmail.com>
2016-04-16 15:46:59 -0400

    topic page fix

--- topic.php
+++ topic.php
@@ -12,10 +12,11 @@
     <title>LearningX | Your perfect Java teacher</title>
     <link rel="stylesheet" href="css/normalize.css">
     
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="css/style.css">  
+    <link rel="stylesheet" href="css/normalize.css">
+
+    <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
+
+        <link rel="stylesheet" href="css/style.css">
   </head>
 
   <body>
