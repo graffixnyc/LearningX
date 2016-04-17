f7b9c3053df4bd4457e0e1556b7eb0e9c030e26b
Patrick Hill <graffixnyc@gmail.com>
2016-04-17 10:46:41 -0400

    Making sure I pushed my last changes

--- menu.php
+++ menu.php
@@ -51,12 +51,12 @@
                 $topics=getTopics();
                 //Loop through the results and display them.. you can write out HTML with them as you see I'm writing out the topic and then the BR HTML tag.  If you need help concatenating the tags let me know
                 foreach($topics as $item) {
-                    if (isset($_GET['id']) && $_GET['id'] == $item["topicID"]) {
-                        echo '<li class="active"><a href=topic?id=' . $item["topicID"] . '>' . $item["topic"] . '</a></li>';
-                        $theTopic = $item["topic"];
-                    } else {
-                        echo '<li><a href=topic?id=' . $item["topicID"] . '>' . $item["topic"] . '</a></li>';
-                    }
+            if (isset($_GET['id']) && $_GET['id'] == $item["topicID"]) {
+                    echo '<li class="active"><a href=topic?id=' . $item["topicID"] . '>' . $item["topic"] . '</a></li>';
+                    $theTopic = $item["topic"];
+                  } else {
+                    echo '<li><a href=topic?id=' . $item["topicID"] . '>' . $item["topic"] . '</a></li>';
+                  }
                 }
             ?>
             <li>
